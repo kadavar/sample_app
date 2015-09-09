b73519069ab508fb77299327f7d343736ff20321
Alexandr <kadavar32@yahoo.com>
2015-09-09 16:16:03 +0300

    Add a full_title_helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe "Static pages" do
-
+    let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -12,8 +12,13 @@ describe "Static pages" do
       
       it "should have the title 'Home'" do
           visit '/static_pages/home'
-          expect(page).to have_title('Home');
+          expect(page).to have_title("#{base_title} | Home");
         end
+      
+      it "should have a custom page title" do
+          visit '/static_pages/home' 
+          expect(page).not_to have_title(' | Home')
+      end 
         end
     
     describe "Help page" do
@@ -25,7 +30,7 @@ describe "Static pages" do
     
       it "shoud have the title 'Help'" do
           visit '/static_pages/help'
-          expect(page).to have_title('Help') 
+          expect(page).to have_title("#{base_title} | Help") 
       end
   end
 
@@ -40,9 +45,21 @@ describe "About page" do
 
     it "should have the title 'About Us'" do
         visit '/static_pages/about'
-        expect(page).to have_title('About Us')
+        expect(page).to have_title("#{base_title}  | About Us")
     end
 
         end
+
+describe "Contact page" do
+    it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_content('Contact')
+    end
+    
+    it"should have the title 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_title("#{base_title} | Contact")
+    end
+end
 
 end
